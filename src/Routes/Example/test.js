173ad124d4cf99be12e32cc865d72dc9dd021026
Yuri Eduardo <57859317+yuridv@users.noreply.github.com>
2023-11-24 12:13:41 -0300

    Update Validate
    Structure changed and added the default option

--- src/Routes/Example/test.js
+++ src/Routes/Example/test.js
@@ -4,12 +4,12 @@ const route = async (req, res) => {
   try {
     let body = await Validate(req.body, {
       string: { required: true, type: 'string' },
-      number: { required: true, type: 'number' },
+      number: { required: true, type: 'number', remove_zeros: true },
       array: { required: true, type: 'array' },
       cpf: { required: true, type: 'cpf' },
       phone: { required: true, type: 'phone' },
       email: { required: true, type: 'email' },
-      date: { required: true, type: 'date' },
+      date: { required: true, type: 'date', default: new Date() },
     })
     return { status: 201, ...body }
   } catch(err) {
--- src/Utils/Functions/Validate.js
+++ src/Utils/Functions/Validate.js
@@ -5,44 +5,32 @@ const route = (body, camps, error = '') => new Promise(async (res,rej) => {
     if (body && camps) {
       
       for (let key in camps) {
+        // DEFINIR VALOR PADRÃO CASO NÃO ESTEJA PREENCHIDO
+        if (!body[key] && camps[key].default) body[key] = camps[key].default
+
         if (!body[key] && camps[key].required) {
-          error += `\n* O Campo '${camps[key].name || key}' não foi preenchido...`
+          error += `\n* O campo '${camps[key].name || key}' não foi preenchido...`
         } else if (body[key] || body[key] == '0') {
-          if (camps[key].type) camps[key].type = camps[key].type.charAt(0).toUpperCase() + camps[key].type.slice(1).toLowerCase();
-
-          if (Types[camps[key].type]) {
-            await Types[camps[key].type](camps, body, key)
-              .catch((e) => error += '\n* ' + e)
-          } else if (camps[key].type) error += '\n* O tipo "' + camps[key].type + '" não é valido...'
-
+          // REMOVER ESPAÇOS DA FRENTE E DO FINAL
           if (typeof body[key] == 'string') body[key] = body[key].trim()
-          if (camps[key].remove_zeros) body[key] = await Zeros(body[key])
 
-          if (camps[key].replace) {
-            for (let i in camps[key].replace) {
-              i = camps[key].replace[i]
-              if (i.includes('/')) {
-                body[key] = body[key].replaceAll(i.split('/')[0], i.split('/')[1])
-              } else body[key] = body[key].replaceAll(i, '')
-            }
-          }
- 
-          if (camps[key].len && String(body[key]).length != camps[key].len) {
-            error += `\n* O Campo '${camps[key].name || key}' precisa conter '${camps[key].length}' carcteres...`
-          }
-          if (camps[key].min && String(body[key]).length < camps[key].min) {
-            error += `\n* O Campo '${camps[key].name || key}' precisa conter no minimo '${camps[key].min}' carcteres...`
-          }
-          if (camps[key].max && String(body[key]).length > camps[key].max) {
-            error += `\n* O Campo '${camps[key].name || key}' precisa conter no maximo '${camps[key].max}' carcteres...`
-          }
+          // REMOVER ZEROS DA FRENTE
+          if (camps[key].remove_zeros) body[key] = await Functions.Zeros(body[key]);
 
-          if (camps[key].include) {
-            if (!camps[key].include.includes(body[key])) {
-              error += `\n* O Campo '${camps[key].name || key}' precisa incluir um desses: '${camps[key].include.join(', ')}'...`
-            }
-          }
+          // SUBSTITUIR PALAVRAS
+          if (camps[key].replace) await Functions.Replace(camps, body, key);
 
+          // VERIFIAR O TIPO
+          if (camps[key].type) camps[key].type = camps[key].type.charAt(0).toUpperCase() + camps[key].type.slice(1).toLowerCase();
+          if (Types[camps[key].type]) {
+            await Types[camps[key].type](camps, body, key).catch((e) => error += '\n* ' + e);
+          } else if (camps[key].type) error += '\n* O tipo "' + camps[key].type + '" do campo "' + (camps[key].name || key) + '" não é valido...'
+
+          // VERIFICAR SE CONTEM UMA PALAVRA
+          if (camps[key].equal) await Functions.Equal(camps, body, key).catch((e) => error += '\n* ' + e);
+
+          // VERIFICAR A QUANTIDADE DE CARACTERES
+          if (camps[key].len || camps[key].min || camps[key].max) await Functions.Length(camps, body, key).catch((e) => error += e);
         }
       }
   
@@ -58,11 +46,44 @@ const route = (body, camps, error = '') => new Promise(async (res,rej) => {
 
 module.exports = route;
 
-// REMOVER ZEROS DA FRENTE
-async function Zeros(v) {
-  if (v.slice(0,1) == "0") v = v.slice(1,v.length);
-  if (v.slice(0,1) == "0") return Zeros(v);
-  return v;
+const Functions = {
+  
+  Zeros: async (value) => {
+    if (String(value).slice(0,1) == "0") value = value.slice(1,value.length);
+    if (String(value).slice(0,1) == "0") return Functions.Zeros(value);
+    return value;
+  },
+
+  Replace: async (camps, body, key) => {
+    for (let i in camps[key].replace) {
+      i = camps[key].replace[i]
+      if (i.includes('/')) {
+        body[key] = String(body[key]).replaceAll(i.split('/')[0], i.split('/')[1])
+      } else body[key] = String(body[key]).replaceAll(i, '')
+    }
+  },
+
+  Equal: async (camps, body, key) => new Promise(async (res,rej) => {
+    if (String(camps[key].include).includes(body[key])) {
+      return res();
+    } else return rej(`O campo '${camps[key].name || key}' precisa ser igual a um desses: '${camps[key].include.join(', ')}'`)
+  }),
+
+  Length: async (camps, body, key) => new Promise(async (res,rej) => {
+    let error = '';
+    if (camps[key].len && String(body[key]).length != camps[key].len) {
+      error += `\n* O campo '${camps[key].name || key}' precisa conter '${camps[key].len}' caracteres...`
+    }
+    if (camps[key].min && String(body[key]).length < camps[key].min) {
+      error += `\n* O campo '${camps[key].name || key}' precisa conter no minimo '${camps[key].min}' caracteres...`
+    }
+    if (camps[key].max && String(body[key]).length > camps[key].max) {
+      error += `\n* O campo '${camps[key].name || key}' precisa conter no maximo '${camps[key].max}' caracteres...`
+    }
+    if (error) return rej(error);
+    return res();
+  })
+
 }
 
 const Types = {
@@ -71,7 +92,7 @@ const Types = {
     body[key] = String(body[key])
     if (body[key] && body[key] !== '[object Object]') {
       return res();
-    } else return rej(`O Campo '${camps[key].name || key}' precisa ser um texto...`);
+    } else return rej(`O campo '${camps[key].name || key}' precisa ser um texto...`);
   }),
 
   Number: async (camps, body, key) => new Promise(async (res,rej) => {
@@ -83,7 +104,7 @@ const Types = {
     body[key] = Number(body[key]);
     if (body[key]) {
       return res();
-    } else return rej(`O Campo '${camps[key].name || key}' precisa conter apenas números...`)
+    } else return rej(`O campo '${camps[key].name || key}' precisa conter apenas números...`)
   }),
 
   Array: async (camps, body, key) => new Promise(async (res,rej) => {
@@ -106,30 +127,30 @@ const Types = {
 
     if (typeof body[key] == 'object' && body[key].length > 0) {
       return res();
-    } else return rej(`O Campo '${camps[key].name || key}' precisa ser uma lista...`)
+    } else return rej(`O campo '${camps[key].name || key}' precisa ser uma lista...`)
   }),
 
   Cpf: async (camps, body, key) => new Promise(async (res,rej) => {
     body[key] = String(body[key]).replaceAll(' ','').replaceAll('.','').replaceAll('-','')
     if (body[key].length <= 11 && Number(body[key])) {
-      body[key] = (("00000000000" + body[key]).slice(-11)).replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+      body[key] = (("00000000000" + body[key]).slice(-11))//.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
       return res();
-    } else return rej(`O Campo '${camps[key].name || key}' não é um CPF valido...! Exemplo: 123.456.789-11`);
+    } else return rej(`O campo '${camps[key].name || key}' não é um CPF valido...! Exemplo: 123.456.789-11`);
   }),
 
   Phone: async (camps, body, key) => new Promise(async (res,rej) => {
     body[key] = String(body[key]).replaceAll(' ','').replaceAll('.','').replaceAll('-','').replaceAll('(','').replaceAll(')','')
     if (body[key].length == 11 && Number(body[key])) {
-      body[key] = Number(body[key]);
+      body[key] = String(body[key]);
       return res();
-    } else return rej(`O Campo '${camps[key].name || key}' não é um Telefone valido...! Exemplo: (12) 9 1234-5678`);
+    } else return rej(`O campo '${camps[key].name || key}' não é um Telefone valido...! Exemplo: (12) 9 1234-5678`);
   }),
 
   Email: async (camps, body, key) => new Promise(async (res,rej) => {
     body[key] = String(body[key]).replaceAll(' ','').toLowerCase()
     if (body[key].includes('@') && body[key].includes('.com')) {
       return res();
-    } else return rej(`O Campo '${camps[key].name || key}' não é um Email valido...! Exemplo: exemplo@gmail.com`);
+    } else return rej(`O campo '${camps[key].name || key}' não é um Email valido...! Exemplo: exemplo@gmail.com`);
   }),
 
   Date: async (camps, body, key) => new Promise(async (res,rej) => {
@@ -138,15 +159,15 @@ const Types = {
       body[key] = new Date(body[key].setHours(body[key].getHours() - 3)).toISOString().split('T')
       let date = body[key][0].split('-').map(r=> Number(r))
       if (date[0] < 1900 || date[0] > 2100 || date[1] < 1 || date[1] > 12 || date[2] < 1 || date[2] > 31) {
-        return rej(`O Campo '${camps[key].name || key}' não possui uma data valida... Data Inserida: ${date.reverse().join('/')}`)
+        return rej(`O campo '${camps[key].name || key}' não possui uma data valida... Data Inserida: ${date.reverse().join('/')}`)
       }
       let time = body[key][1].split('.')[0].split(':').map(r=> Number(r))
       if (time[0] < 0 || time[0] > 23 || time[1] < 0 || time[1] > 59 || time[2] < 0 || time[2] > 59) {
-        return rej(`O Campo '${camps[key].name || key}' não possui uma horario valido... Data Inserida: ${date.reverse().join('/')}`)
+        return rej(`O campo '${camps[key].name || key}' não possui uma horario valido... Data Inserida: ${date.reverse().join('/')}`)
       }
       body[key] = new Date(body[key].join('T'));
       return res();
-    } else return rej(`O Campo '${camps[key].name || key}' foi preenchido com uma data invalida...`);
+    } else return rej(`O campo '${camps[key].name || key}' foi preenchido com uma data invalida...`);
   })
 
 }
