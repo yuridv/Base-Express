4ab0eec197d1df123534d18e6b42b210a0f4f681
Yuri Eduardo <57859317+yuridv@users.noreply.github.com>
2023-12-14 21:30:23 -0300

    test

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Express = require('express');
 const express = Express();
 
 const server = require('http').createServer(express);
+console.log('test')
 
 const routes = require('./src/Routes/routes');
 
